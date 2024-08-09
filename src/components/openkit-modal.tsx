c0e33406326d48f6c7f83bdf33fdce62470bf225
Veri5ied <alvinuchenna14@gmail.com>
2024-08-09 15:57:15 +0100

    WIP: connections

--- src/components/openkit-modal.tsx
+++ src/components/openkit-modal.tsx
@@ -8,7 +8,8 @@ import {
 import WalletOptions from "./wallet-options";
 import { ReactNode } from "react";
 import { useTheme } from "@/context/theme";
-import { useAddress, useLogout } from "@/hooks";
+import { useAddress } from "@/hooks";
+import { useLogout } from "@/hooks";
 import { useGetConnector } from "@/context/connector";
 
 interface OpenKitModalTriggerProps {
@@ -22,39 +23,81 @@ const OpenKitModal = ({ trigger }: OpenKitModalTriggerProps) => {
   const { address, isConnected, isConnecting } = account;
   const logout = useLogout();
 
-  console.log(address);
-  console.log(connector);
-
   return (
-    <Dialog>
-      <DialogTrigger>{trigger}</DialogTrigger>
-      <DialogContent
-        className={`${
-          theme === "dark" ? "bg-[#0c0e13]" : "bg-[#fff]"
-        } w-full md:w-[360px] min-h-[300px] outline-none`}
-      >
-        <DialogHeader className="mb-2">
-          <DialogTitle>Log in or Sign up</DialogTitle>
-        </DialogHeader>
-        <p className="text-sm">
-          Log in or sign up by selecting a wallet connection method below.
-        </p>
-        <div className="grid gap-2 mt-[20px]">
-          <WalletOptions />
-        </div>
-        <div className="text-sm pt-4">
-          Opensource project by{" "}
-          <a
-            href="https://github.com/Veri5ied/openkit"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-orange-600"
+    <div>
+      {isConnecting && (
+        <Dialog open={true}>
+          <DialogTrigger />
+          <DialogContent
+            className={`${
+              theme === "dark" ? "bg-[#0c0e13]" : "bg-[#fff]"
+            } w-full md:w-[340px] min-h-[200px] outline-none`}
           >
-            Openkit
-          </a>
+            <div className="grid place-items-center gap-2 text-center py-4">
+              <img
+                src={
+                  connector?.icon
+                    ? connector?.icon
+                    : connector?.name === "Coinbase Wallet"
+                    ? "https://res.cloudinary.com/dfymeyybz/image/upload/v1723121507/openkit/xrflg2a28vfc36ovqrjh.png"
+                    : connector?.name === "WalletConnect"
+                    ? "https://res.cloudinary.com/dfymeyybz/image/upload/v1723121678/openkit/k3mhkjpghebz95gs6dea.png"
+                    : ""
+                }
+                alt="_"
+                className="w-[50px] h-[50px] rounded-md"
+              />
+              <h1 className="font-semibold text-lg">
+                Opening {connector?.name}...{" "}
+              </h1>
+              <p className="text-base font-normal">
+                Confirm connection in the extension
+              </p>
+              <div className="spinner w-[30px]" />
+            </div>
+          </DialogContent>
+        </Dialog>
+      )}
+      {!isConnecting && !isConnected && (
+        <div>
+          <Dialog>
+            <DialogTrigger>{trigger}</DialogTrigger>
+            <DialogContent
+              className={`${
+                theme === "dark" ? "bg-[#0c0e13]" : "bg-[#fff]"
+              } w-full md:w-[360px] min-h-[300px] outline-none`}
+            >
+              <DialogHeader className="mb-2">
+                <DialogTitle>Log in or Sign up</DialogTitle>
+              </DialogHeader>
+              <p className="text-sm">
+                Log in or sign up by selecting a wallet connection method below.
+              </p>
+              <div className="grid gap-2 mt-[20px]">
+                <WalletOptions />
+              </div>
+              <div className="text-sm pt-4">
+                Opensource project by{" "}
+                <a
+                  href="https://github.com/Veri5ied/openkit"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-orange-600"
+                >
+                  Openkit
+                </a>
+              </div>
+            </DialogContent>
+          </Dialog>
         </div>
-      </DialogContent>
-    </Dialog>
+      )}
+
+      {address && (
+        <button onClick={() => logout()}>
+          {address} <br /> Logout
+        </button>
+      )}
+    </div>
   );
 };
 
