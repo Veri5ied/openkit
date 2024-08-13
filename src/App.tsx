46a7e7bf3de93a90700c6bbbabdf8405eda64682
Veri5ied <alvinuchenna14@gmail.com>
2024-08-13 09:46:27 +0100

    Enhancement: easy wallet adapters

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+//import "./App.css";
 import OpenKitModal from "./components/openkit-modal";
 
 function App() {
--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import OpenKitProvider from "@/provider/OpenkitProvider";
-import { coinbaseWallet, metaMask, walletConnect } from "wagmi/connectors";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <OpenKitProvider
-      connectors={[
-        metaMask(),
-        coinbaseWallet(),
-        walletConnect({ projectId: "" }),
-      ]}
       theme="light"
+      supportedWallets={["metamask", "coinbase wallet"]}
     >
       <App />
     </OpenKitProvider>
