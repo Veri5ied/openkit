d9f3b8009eeefe56045d3fc7863395b2d56347c7
Veri5ied <alvinuchenna14@gmail.com>
2024-08-14 09:50:41 +0100

    CLNUP: removed unused vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-//import "./App.css";
 import OpenKitModal from "./components/openkit-modal";
 
 function App() {
-  return (
-    <>
-      <OpenKitModal trigger={<button>Open Now</button>} />
-    </>
-  );
+  return <OpenKitModal trigger={<button>Open Now</button>} />;
 }
 
 export default App;
